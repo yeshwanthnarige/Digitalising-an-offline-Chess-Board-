e931d74d41e55c931d0ca6a71f6e38ea69abbe21
Yeshwanth Narige <61676569+yeshwanthnarige@users.noreply.github.com>
2022-02-04 11:15:29 +0530

    Add files via upload

--- models/model2.py
+++ models/model2.py
@@ -5,7 +5,7 @@ from keras.layers import Input
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D, MaxPool2D
 
-img_input = Input((224,224,3))
+img_input = Input((224, 224, 3))
 
 
 x = Conv2D(64, (3, 3), activation='relu', padding='same',
@@ -43,18 +43,18 @@ x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 x = Conv2D(512, (3, 3), activation='relu',
            padding='same', name='block5_conv1')(x)
 x = Conv2D(512, (3, 3), activation='relu',
-            padding='same', name='block5_conv2')(x)
+           padding='same', name='block5_conv2')(x)
 x = Conv2D(512, (3, 3), activation='relu',
-            padding='same', name='block5_conv3')(x)
+           padding='same', name='block5_conv3')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
 # Classification block
 x = Flatten(name='flatten')(x)
-x = Dense(500, activation='relu', name='fc1')(x)
-x = Dense(500, activation='relu', name='fc2')(x)
+x = Dense(500, activation='sigmoid', name='fc1')(x)
+x = Dense(500, activation='sigmoid', name='fc2')(x)
 x = Dense(13, activation='softmax', name='predictions')(x)
 
 
-model1 = Model(input=img_input, outputs=x)
+model2 = Model(input=img_input, outputs=x)
 
 #now our model is ready we should compile and train it
--- models/model3.py
+++ models/model3.py
@@ -1,13 +1,14 @@
 from keras.models import Model
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.layers import Input
+from keras.layers import Flatten, Dense, Input
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D, MaxPool2D
 
-img_input = Input((224,224,3))
 
+input_size = (224, 224, 3)
 
+img_input = Input(input_size)
+
+# Block 1
 x = Conv2D(64, (3, 3), activation='relu', padding='same',
            name='block1_conv1')(img_input)
 x = Conv2D(64, (3, 3), activation='relu',
@@ -28,6 +29,8 @@ x = Conv2D(256, (3, 3), activation='relu',
            padding='same', name='block3_conv2')(x)
 x = Conv2D(256, (3, 3), activation='relu',
            padding='same', name='block3_conv3')(x)
+x = Conv2D(256, (3, 3), activation='relu',
+           padding='same', name='block3_conv4')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
 # Block 4
@@ -37,16 +40,21 @@ x = Conv2D(512, (3, 3), activation='relu',
            padding='same', name='block4_conv2')(x)
 x = Conv2D(512, (3, 3), activation='relu',
            padding='same', name='block4_conv3')(x)
+x = Conv2D(512, (3, 3), activation='relu',
+           padding='same', name='block4_conv4')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
 # Block 5
 x = Conv2D(512, (3, 3), activation='relu',
            padding='same', name='block5_conv1')(x)
 x = Conv2D(512, (3, 3), activation='relu',
-            padding='same', name='block5_conv2')(x)
+           padding='same', name='block5_conv2')(x)
 x = Conv2D(512, (3, 3), activation='relu',
-            padding='same', name='block5_conv3')(x)
+           padding='same', name='block5_conv3')(x)
+x = Conv2D(512, (3, 3), activation='relu',
+           padding='same', name='block5_conv4')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
 
 # Classification block
 x = Flatten(name='flatten')(x)
@@ -54,7 +62,4 @@ x = Dense(500, activation='relu', name='fc1')(x)
 x = Dense(500, activation='relu', name='fc2')(x)
 x = Dense(13, activation='softmax', name='predictions')(x)
 
-
-model1 = Model(input=img_input, outputs=x)
-
-#now our model is ready we should compile and train it
+model3 = Model(input=img_input, outputs=x)
